1eb7ace929e6faa877bda5f5c4d3273c28e1f2f6
Alexandre Pontes <alexandrepontesgyn@gmail.com>
2020-03-21 18:50:48 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/alexandrepontes/cursomc/domain/Cliente.java
+++ src/main/java/com/alexandrepontes/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.alexandrepontes.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 public class Cliente implements Serializable {
 
@@ -27,11 +28,12 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipoCliente;
 
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
-	@CollectionTable(name="telefone")
+	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
 	public Cliente() {
