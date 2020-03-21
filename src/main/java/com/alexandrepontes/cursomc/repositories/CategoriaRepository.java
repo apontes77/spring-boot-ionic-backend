b502de93f0bc8b5a626b8f9ea81ef6031b2d1c61
Alexandre Pontes <alexandrepontesgyn@gmail.com>
2020-03-21 14:54:15 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/alexandrepontes/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.alexandrepontes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexandrepontes.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+		
+}
