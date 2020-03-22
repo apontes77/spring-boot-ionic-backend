1a8625448f810758c0edd4d29216d2de0e70932b
Alexandre Pontes <alexandrepontesgyn@gmail.com>
2020-03-21 21:41:40 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/alexandrepontes/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.alexandrepontes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandrepontes.cursomc.domain.Pedido;
+import com.alexandrepontes.cursomc.repositories.PedidoRepository;
+import com.alexandrepontes.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
